d45d07cea9a7440784f774ff1cef76344042b468
TestAquatic <you@example.com>
2024-09-26 11:24:27 +0900

    generic.go 작성

---
+++ generic/generic.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func print[T any](s []T) {
+	for _, v := range s {
+		fmt.Print(v, " ")
+	}
+	fmt.Println()
+}
+
+func main() {
+	Ints := []int{1, 2, 3}
+	Strings := []string{"One", "Two", "Three"}
+	print(Ints)
+	print(Strings)
+}
