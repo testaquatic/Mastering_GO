1cee82bb026b3a572da9dd9d852c1d1edd9e2917
TestAquatic <you@example.com>
2024-10-04 03:02:10 +0900

    capLen

---
+++ ch02/capLen/capLen.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := make([]int, 4)
+	fmt.Println("L:", len(a), "C:", cap(a))
+	b := []int{0, 1, 2, 3, 4}
+	fmt.Println("L:", len(b), "C:", cap(b))
+
+	aSlice := make([]int, 4, 4)
+	fmt.Println(aSlice)
+
+	aSlice = append(aSlice, 5)
+	fmt.Println(aSlice)
+	fmt.Println("L:", len(aSlice), "C", cap(aSlice))
+	aSlice = append(aSlice, []int{-1, -2, -3, -4}...)
+
+	fmt.Println(aSlice)
+	fmt.Println("L:", len(aSlice), "C:", cap(aSlice))
+}
