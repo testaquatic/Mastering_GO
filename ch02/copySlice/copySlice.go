3835c801330f95ec1fec2ee7947ef2b4b5e7d8aa
TestAquatic <you@example.com>
2024-10-11 01:29:11 +0900

    copySlice

---
+++ ch02/copySlice/copySlice.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	a1 := []int{1}
+	a2 := []int{-1, -2}
+	a5 := []int{10, 11, 12, 13, 14}
+	fmt.Println("a1", a1)
+	fmt.Println("a2", a2)
+	fmt.Println("a5", a5)
+
+	copy(a1, a2)
+	fmt.Println("a1", a1)
+	fmt.Println("a2", a2)
+
+	copy(a1, a5)
+	fmt.Println("a1", a1)
+	fmt.Println("a5", a5)
+
+	copy(a5, a2)
+	fmt.Println("a2", a2)
+	fmt.Println("a5", a5)
+}
