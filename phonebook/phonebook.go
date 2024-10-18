652417c12e3576090dae8f04f760d6d4db0379d9
TestAquatic <you@example.com>
2024-10-19 01:51:37 +0900

    phonebook 중간 저장

--- phonebook/phonebook.go
+++ phonebook/phonebook.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
-	"math/rand"
+	"io"
 	"os"
-	"path/filepath"
-	"strconv"
+	"regexp"
+	"strings"
+	"time"
 )
 
 type Entry struct {
@@ -15,10 +17,103 @@ type Entry struct {
 	// 성
 	Surname string
 	// 전화번호
-	Tel string
+	Tel        string
+	LastAccess string
+}
+
+func initS(name, surname, tel string) *Entry {
+	return &Entry{
+		Name:       name,
+		Surname:    surname,
+		Tel:        tel,
+		LastAccess: fmt.Sprint(time.Now().Unix()),
+	}
 }
 
 var data = []Entry{}
+
+const CSVFILE = "phonebook.csv"
+
+var index map[string]int
+
+func readCSVFILE(filepath string) error {
+	_, err := os.Stat(CSVFILE)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Open(CSVFILE)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	for line, err := csvReader.Read(); err == nil; line, err = csvReader.Read() {
+		temp := Entry{
+			Name:       line[0],
+			Surname:    line[1],
+			Tel:        line[2],
+			LastAccess: fmt.Sprint(time.Now().Unix()),
+		}
+		data = append(data, temp)
+	}
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
+
+func saveCSVFILE(filepath string) error {
+	csvfile, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer csvfile.Close()
+
+	csvWriter := csv.NewWriter(csvfile)
+	defer csvWriter.Flush()
+	for _, row := range data {
+		temp := []string{row.Name, row.Surname, row.Tel, row.LastAccess}
+		err = csvWriter.Write(temp)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func createIndex() error {
+	index = make(map[string]int)
+	for i, k := range data {
+		key := k.Tel
+		index[key] = i
+	}
+
+	return nil
+}
+
+func matchTel(t string) bool {
+	r := regexp.MustCompile(`^/d+$`)
+	return r.Match([]byte(t))
+}
+
+func insert(pS *Entry) error {
+	_, ok := index[(*pS).Tel]
+	if ok {
+		return fmt.Errorf("%s already exists", pS.Tel)
+	}
+	data = append(data, *pS)
+	_ = createIndex()
+
+	err := saveCSVFILE(CSVFILE)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func search(key string) int64 {
 	count := int64(0)
@@ -38,68 +133,66 @@ func list() {
 	}
 }
 
-func random(min, max int) int {
-	return rand.Intn(max-min) + min
-}
-
-const (
-	MIN = 0
-	MAX = 94
-)
-
-func getString(len int64) string {
-	temp := ""
-	startChar := "!"
-	var i int64 = 1
-	for {
-		myRand := random(MIN, MAX)
-		newChar := string(startChar[0] + byte(myRand))
-		temp = temp + newChar
-		if i == len {
-			break
-		}
-		i++
-	}
-	return temp
-}
-
-func populate(n int) {
-	for i := 0; i < n; i++ {
-		name := getString(4)
-		surname := getString(5)
-		n := strconv.Itoa(random(100, 199))
-		data = append(data, Entry{name, surname, n})
-	}
-}
-
-type Args struct {
-	Search string
-	List   bool
-}
-
-var args = Args{}
-
-func init() {
-	flag.BoolVar(&args.List, "list", false, "list entries from phonebook.")
-	flag.StringVar(&args.Search, "search", "", "search entries from phonebook.")
-	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "%s: a phonebook\n", filepath.Base(os.Args[0]))
-		flag.PrintDefaults()
-	}
-}
-
 func main() {
 	flag.Parse()
-	if len(os.Args) == 1 {
-		flag.Usage()
+
+	if flag.NArg() == 0 {
+		fmt.Println("Usage: insert|delete|search|list <arguments>")
 		return
 	}
 
-	populate(100)
+	_, err := os.Stat(CSVFILE)
+	if err != nil {
+		fmt.Println("Creating", CSVFILE)
+		f, err := os.Create(CSVFILE)
+		if err != nil {
+			f.Close()
+			fmt.Println(err)
+			return
+		}
+		f.Close()
+	}
+
+	fileInfo, err := os.Stat(CSVFILE)
+	mode := fileInfo.Mode()
+	if !mode.IsRegular() {
+		fmt.Println(CSVFILE, "not a regular file!")
+		return
+	}
+
+	err = readCSVFILE(CSVFILE)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = createIndex()
+	if err != nil {
+		fmt.Println("Cannot create index.")
+		return
+	}
 
 	fmt.Println("Data has", len(data), "entries.")
 
-	switch {
+	switch flag.Arg(0) {
+	case "insert":
+		if flag.NArg() != 4 {
+			fmt.Println("Usage: insert Name Surname Telephone")
+			return
+		}
+		t := strings.ReplaceAll(flag.Arg(3), "-", "")
+		if !matchTel(t) {
+			fmt.Println("Not a valid telephone number:", t)
+			return
+		}
+		temp := initS(flag.Arg(1), flag.Arg(2), t)
+		if temp != nil {
+			err := insert(temp)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
 	// search 커맨드
 	case args.Search != "":
 		if len(os.Args) != 3 {
