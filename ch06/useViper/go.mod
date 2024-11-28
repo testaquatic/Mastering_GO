d8ef30a6dd891d43c9c1ec8406aa111bf0f3755f
Your Name <you@example.com>
2024-11-28 22:57:22 +0900

    6장 중간 백업

---
+++ ch06/useViper/go.mod
@@ -0,0 +1,3 @@
+module github.com/TestAquatic/Mastering_GO/ch06/useViper
+
+go 1.23.3
--- ch06/yml/yml.go
+++ ch06/yml/yml.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"gopkg.in/yaml.v3"
+)
+
+var yamlfile = `
+image: Golang
+matrix:
+  docker: python
+  version: [2.7, 3.9]
+`
+
+type Mat struct {
+	DockerImage string    `yaml:"docker"`
+	Version     []float32 `yaml:",flow"`
+}
+
+type Yaml struct {
+	Image  string
+	Matrix Mat
+}
+
+func main() {
+	var yamlUnmarshal Yaml
+	err := yaml.Unmarshal([]byte(yamlfile), &yamlUnmarshal)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	fmt.Println("After Unmarshal (Structure):")
+	fmt.Println(yamlUnmarshal)
+
+	yamlMarshal, err := yaml.Marshal(&yamlUnmarshal)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("After Marshal (YAML code)")
+	fmt.Println(string(yamlMarshal))
 }
