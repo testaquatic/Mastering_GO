5256b3268d123c1f7a11917a380014279e8f11f0
TestAquatic <86832451+testaquatic@users.noreply.github.com>
2025-02-26 22:30:23 +0900

    독서 완료

---
+++ A/GC/mapNoStar.go
@@ -0,0 +1,14 @@
+package main
+
+import "runtime"
+
+func main() {
+	N := 80_000_000
+	myMap := make(map[int]int)
+	for i := 0; i < N; i++ {
+		value := int(i)
+		myMap[value] = value
+	}
+	runtime.GC()
+	_ = myMap[0]
+}
---
+++ A/GC/mapSplit.go
@@ -0,0 +1,18 @@
+package main
+
+import "runtime"
+
+func main() {
+	N := 80_000_000
+
+	split := make([]map[int]int, 2000)
+	for i := range split {
+		split[i] = make(map[int]int)
+	}
+	for i := 0; i < N; i++ {
+		value := int(i)
+		split[i%2000][value] = value
+	}
+	runtime.GC()
+	_ = split[0][0]
+}
